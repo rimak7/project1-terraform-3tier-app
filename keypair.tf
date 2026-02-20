65c9ede16bd8f93cc00e8f594857afa2487ee4b6
rima <armande.kanm7@gmail.com>
2026-02-19 22:44:54 -0500

    Fix: Remove ghost submodule reference

--- keypair.tf
+++ keypair.tf
@@ -2,13 +2,16 @@ resource "tls_private_key" "utc_key" {
   rsa_bits  = 4096
   algorithm = "RSA"
 }
-resource "local_file" "github_key" {
-  filename        = "${aws_key_pair.utc_key.key_name}.pem"
+
+# Renamed to match the 'depends_on' in your ec2.tf
+resource "local_file" "utc_pem" {
+  filename        = "${var.utc_key}.pem"
   content         = tls_private_key.utc_key.private_key_pem
   file_permission = "0400"
 }
-resource "aws_key_pair" "utc_key" {
+
+# Renamed to match the 'key_name' reference in your ec2.tf
+resource "aws_key_pair" "utc_key_pair" {
   public_key = tls_private_key.utc_key.public_key_openssh
   key_name   = var.utc_key
 }
-
