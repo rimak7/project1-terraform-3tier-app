7ee77c03e6f2fad31fa59730b7c3bd899da455d6
rima <armande.kanm7@gmail.com>
2026-02-19 22:41:36 -0500

    Fix: Remove ghost submodule reference

--- ec2.tf
+++ ec2.tf
@@ -1,34 +1,32 @@
 # 1. Bastion Host (Public Subnet)
 resource "aws_instance" "bastion" {
-  ami                         = data.aws_ami.amazon.id
-  instance_type               = "t2.micro"
-  # FIXED: Matches the key name in your loop
+  ami                         = var.ami_id # Use variable instead of data source
+  instance_type               = var.instance_type # Use variable
   subnet_id                   = aws_subnet.pub["1a"].id 
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   key_name                    = aws_key_pair.utc_key_pair.key_name
   associate_public_ip_address = true
   
-  # CRITICAL: Ensures the .pem file is created on the runner BEFORE provisioners run
   depends_on = [local_file.utc_pem]
 
   user_data = file("user.sh")
 
   connection {
     type        = "ssh"
-    user        = "ec2-user"
+    user        = "ec2-user" # Assuming Amazon Linux 2 based on your AMI default
     private_key = tls_private_key.utc_key.private_key_pem
     host        = self.public_ip
-    timeout     = "2m" # Gives the instance time to boot
+    timeout     = "2m"
   }
 
   provisioner "file" {
-    source      = "./utc-key.pem"
-    destination = "/home/ec2-user/utc-key.pem"
+    source      = "./${aws_key_pair.utc_key_pair.key_name}.pem"
+    destination = "/home/ec2-user/${aws_key_pair.utc_key_pair.key_name}.pem"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod 400 /home/ec2-user/utc-key.pem"
+      "chmod 400 /home/ec2-user/${aws_key_pair.utc_key_pair.key_name}.pem"
     ]
   }
 
@@ -39,11 +37,10 @@ resource "aws_instance" "bastion" {
   }
 }
 
-# 2. Private Server (Private Subnet)
+# 2. Private Server
 resource "aws_instance" "private_server" {
-  ami                    = data.aws_ami.amazon.id
-  instance_type          = "t3.micro"
-  # FIXED: Pointing to your private subnet map
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.priv["1a"].id 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name               = aws_key_pair.utc_key_pair.key_name
@@ -52,14 +49,5 @@ resource "aws_instance" "private_server" {
     Name = "private-app-server"
     env  = "dev"
     team = "config management"
-  }
-}
-
-data "aws_ami" "amazon" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
--- variable.tf
+++ variable.tf
@@ -1,21 +1,42 @@
-variable "utc_key" {
-  description = "Name of the SSH key pair to use for EC2 instances"
-  type        = string
-  default     = "my-ssh-key"
-
-}
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
+
+variable "utc_key" {
+  description = "Name of the SSH key pair to use for EC2 instances"
+  type        = string
+  default     = "utc-key"
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
+
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
-  default     = "ami-0f3caa1cf4417e51b" 
+  default     = "ami-0f3caa1cf4417e51b" # Amazon Linux 2 AMI
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+# Environment tags for consistency
+variable "env" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "team" {
+  description = "Team responsible for the infrastructure"
+  type        = string
+  default     = "config management"
 }
