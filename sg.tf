b30011048ff12cc2439fbd3a12a609e462dec433
rima <armande.kanm7@gmail.com>
2026-02-19 21:18:19 -0500

    add files on main

--- sg.tf
+++ sg.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_IP_HERE/32"] # Be sure to update this!
+    cidr_blocks = ["0.0.0.0/0"] # Be sure to update this!
   }
 
   egress {
